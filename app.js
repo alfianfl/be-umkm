bb362dc44fd866c31feb837b072239707e259fd6
alfianfl <alvian.wadada@gmail.com>
2022-06-16 23:51:50 +0700

    first commitan

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// app.use(cors({ credentials: true, origin: process.env.CLIENT_URL,  optionSuccessStatus: 200 }));
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL,  optionSuccessStatus: 200 }));
 
 
 app.use("/api", apiRoutes);
